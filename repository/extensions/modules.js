56c076db3419172844139a3a225e0ca755943f4f
ThuF <jordan.p.pavlov@gmail.com>
2020-04-05 17:55:14 +0300

    Adds "api-modules" Extension

---
+++ repository/extensions/modules.js
@@ -0,0 +1,9 @@
+exports.getContent = function() {
+	return [{
+		name: "repository/v4/content",
+		description: "Repository Content API"
+	}, {
+		name: "repository/v4/manager",
+		description: "Repository Manager API"
+	}];
+};
